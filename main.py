25f26ae5b409e6318558b8ef227c9b1d82301a1f
LucasWu91 <lucas.wu@edu.esiee.fr>
2024-09-27 07:42:45 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+"""
+Fichier main.py
+"""
 #### Imports et définition des variables globales
-import random
 
+# import random
 FILENAME = "listes.csv"
 
 #### Fonctions secondaires
@@ -15,36 +18,61 @@ def read_data(filename):
         list: le contenu du fichier (1 list par ligne)
     """
     l = []
+    with open(filename, mode='r', encoding='utf8') as f:
+        for line in f:
+            l.append(list(map(int, line.strip().split(';'))))
     return l
 
 def get_list_k(data, k):
-    l = []
-    return l
+    """
+    Retourne la k-ieme ligne de la liste
+    """
+    return data[k]
 
 def get_first(l):
-    return None
+    """
+    Retourne le premier element de la liste
+    """
+    return l[0]
 
 def get_last(l):
-    return None
+    """
+    Retourne le dernier element de la liste
+    """
+    return l[-1]
 
 def get_max(l):
-    return None
+    """
+    Retourne le max element de la liste
+    """
+    return max(l)
 
 def get_min(l):
-    return None
+    """
+    Retourne le min element de la liste
+    """
+    return min(l)
 
 def get_sum(l):
-    return None
+    """
+    Retourne la somme de la liste
+    """
+    somme = 0
+    for i in l:
+        somme = somme + i
+    return somme
 
 
 #### Fonction principale
 
 
 def main():
-    pass
-    # data = read_data(FILENAME)
-    # for i, l in enumerate(data):
-    #     print(i, l)
+    """
+    Définition du main
+    """
+    data = read_data(FILENAME)
+    for i, l in enumerate(data):
+        print(i, l)
     # k = 37
     # print(k, get_list_k(data, 37))
 
